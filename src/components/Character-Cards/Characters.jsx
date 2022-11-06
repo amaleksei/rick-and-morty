cfd6f99015da97a5a691d3b792eb243a197178d8
amaleksei-burmiac <zh.burmiac@gmail.com>
2022-11-07 02:48:57 +0800

    Добавлен вывод карточек персонажей

---
+++ src/components/Character-Cards/Characters.jsx
@@ -0,0 +1,19 @@
+import CharacterCard from './Character-Card'
+import { v4 as uuidv4 } from 'uuid'
+
+export default function Characters({ characters }) {
+  const { results } = characters
+  if (results.length > 0) {
+    return (
+      <section className='w-full px-[27px] py-8'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 auto-rows-auto'>
+          {results.map(card => (
+            <CharacterCard key={uuidv4()} card={card} />
+          ))}
+        </div>
+      </section>
+    )
+  }
+
+  return null
+}
